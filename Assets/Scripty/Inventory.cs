55e997384fc329bb18ed294795d80d33eb0bd53d
torelieu <simon1.navratil@gmail.com>
2024-01-09 11:53:40 +0100

    objekty v inventáři

--- Assets/Scripty/Inventory.cs
+++ Assets/Scripty/Inventory.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    public GameObject Sword;
+    public GameObject Heal;
     public GameObject[] inventorySlots; // Array to hold your inventory slots
     public int highlightedSlotIndex = 0; // Index of the currently highlighted slot
+    private bool slot1;
+    private bool slot2;
+    private bool nonSelected;
 
+    private void Start()
+    {
+        slot1 = true; 
+        slot2 = false;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -15,18 +26,34 @@ public class Inventory : MonoBehaviour
 
         // Update visual feedback for highlighted slot
         UpdateHighlight();
+
+        if (slot1 == true)
+        {
+            Sword.SetActive(true);
+            Heal.SetActive(false);
+        }
+        else if (slot2 == true)
+        {
+            Sword.SetActive(false);
+            Heal.SetActive(true);
+        }
     }
 
     void HandleInput()
     {
+        
         // Change the highlighted slot index based on user input
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             highlightedSlotIndex = 0;
+            slot1 = true;
+            slot2 = false;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             highlightedSlotIndex = 1;
+            slot1 = false;
+            slot2 = true;
         }
 
         // Ensure the highlighted index stays within the bounds of the inventory array
