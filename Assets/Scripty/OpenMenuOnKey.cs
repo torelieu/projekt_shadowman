3ec6502980283c8a14a87e1acbc776b43e8214fd
torelieu <simon1.navratil@gmail.com>
2024-01-12 02:49:50 +0100

    menu_animace

--- Assets/Scripty/OpenMenuOnKey.cs
+++ Assets/Scripty/OpenMenuOnKey.cs
@@ -5,13 +5,37 @@ using UnityEngine.UI;
 
 public class OpenMenuOnKey : MonoBehaviour
 {
-    public Image Menu;
+    public GameObject panel;
+    public Animator menuAnimator;
+    private bool a = false;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && a == false)
         {
-            Menu.GetComponent<Animator>().Play("idk");
+            
+            // Trigger the animation when the Escape key is pressed
+            menuAnimator.SetTrigger("ShowMenu");
+            a = true;
         }
+
+        else if (Input.GetKeyDown(KeyCode.Escape) && a == true)
+        {
+            menuAnimator.SetTrigger("DisMenu");
+            a = false;
+            
+        }
+
+       
+    }
+
+    public void DisablePanel()
+    {
+        panel.SetActive(false);
+    }
+
+    public void EnablePanel()
+    {
+        panel.SetActive(true);
     }
 }
